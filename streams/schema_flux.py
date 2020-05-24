70c473d1704412c9c873a0f8012d98aa1e714f80
az365 <artemcarr@yandex.ru>
2020-05-24 23:49:10 +0300

    SchemaFlux: schema.SchemaFlux supported

--- streams/schema_flux.py
+++ streams/schema_flux.py
@@ -1,12 +1,14 @@
 try:  # Assume we're a sub-module in a package.
     import fluxes as fx
     from utils import (
+        schema as sh,
         functions as fs,
         log_progress,
     )
 except ImportError:  # Apparently no higher-level package has been imported, fall back to a local import.
     from .. import fluxes as fx
     from ..utils import (
+        schema as sh,
         functions as fs,
         log_progress,
     )
@@ -45,33 +47,38 @@ def check_rows(rows, schema, skip_errors=False):
 
 
 def apply_schema_to_row(row, schema, skip_bad_values=False, logger=None):
-    for c, (value, description) in enumerate(zip(row, schema)):
-        field_type = description[TYPE_POS]
-        try:
-            cast_function = fs.cast(field_type)
-            new_value = cast_function(value)
-        except ValueError as e:
-            field_name = description[NAME_POS]
-            if logger:
-                message = 'Error while casting field {} ({}) with value {} into type {}'.format(
-                    field_name, c,
-                    value, field_type,
-                )
-                logger.log(msg=message, level=log_progress.LoggingLevel.Error.value)
-            if skip_bad_values:
+    if isinstance(schema, sh.SchemaDescription):
+        converters = schema.get_converters('str', 'py')
+        return [converter(value) for value, converter in zip(row, converters)]
+    elif isinstance(schema, (list, tuple)):
+        for c, (value, description) in enumerate(zip(row, schema)):
+            field_type = description[TYPE_POS]
+            try:
+                cast_function = fs.cast(field_type)
+                new_value = cast_function(value)
+            except ValueError as e:
+                field_name = description[NAME_POS]
                 if logger:
-                    message = 'Skipping bad value in row:'.format(list(zip(row, schema)))
-                    logger.log(msg=message, level=log_progress.LoggingLevel.Debug.value)
-                new_value = None
-            else:
-                message = 'Error in row: {}...'.format(str(list(zip(row, schema)))[:80])
-                if logger:
-                    logger.log(msg=message, level=log_progress.LoggingLevel.Warning.value)
+                    message = 'Error while casting field {} ({}) with value {} into type {}'.format(
+                        field_name, c,
+                        value, field_type,
+                    )
+                    logger.log(msg=message, level=log_progress.LoggingLevel.Error.value)
+                if skip_bad_values:
+                    if logger:
+                        message = 'Skipping bad value in row:'.format(list(zip(row, schema)))
+                        logger.log(msg=message, level=log_progress.LoggingLevel.Debug.value)
+                    new_value = None
                 else:
-                    log_progress.get_logger().show(message)
-                raise e
-        row[c] = new_value
-    return row
+                    message = 'Error in row: {}...'.format(str(list(zip(row, schema)))[:80])
+                    if logger:
+                        logger.log(msg=message, level=log_progress.LoggingLevel.Warning.value)
+                    else:
+                        log_progress.get_logger().show(message)
+                    raise e
+            row[c] = new_value
+        return row
+    return TypeError
 
 
 class SchemaFlux(fx.RowsFlux):
@@ -124,17 +131,32 @@ class SchemaFlux(fx.RowsFlux):
 
     def schematize(self, schema, skip_bad_rows=False, skip_bad_values=False, verbose=True):
         def apply_schema_to_rows(rows):
-            for r in rows:
-                if skip_bad_rows:
-                    try:
-                        yield apply_schema_to_row(r, schema, skip_bad_values=False, logger=self if verbose else None)
-                    except ValueError:
-                        self.log(['Skip bad row:', r], verbose=verbose)
-                else:
-                    yield apply_schema_to_row(r, schema, skip_bad_values, logger=self if verbose else None)
+            if isinstance(schema, sh.SchemaDescription):
+                converters = schema.get_converters('str', 'py')
+                for row in rows:
+                    converted_row = tuple()
+                    for value, converter in zip(row, converters):
+                        converted_value = converter(value)
+                        converted_row.append(converted_value)
+                    yield converted_row.copy()
+            else:
+                for r in rows:
+                    if skip_bad_rows:
+                        try:
+                            yield apply_schema_to_row(r, schema, skip_bad_values=False, logger=self if verbose else None)
+                        except ValueError:
+                            self.log(['Skip bad row:', r], verbose=verbose)
+                    else:
+                        yield apply_schema_to_row(r, schema, skip_bad_values, logger=self if verbose else None)
         return SchemaFlux(
             apply_schema_to_rows(self.data),
             count=None if skip_bad_rows else self.count,
             check=False,
             schema=schema,
         )
+
+    def get_columns(self):
+        if isinstance(self.schema, sh.SchemaDescription):
+            return self.schema.get_columns()
+        elif isinstance(self.schema, (list, tuple)):
+            return [c[0] for c in self.schema]
