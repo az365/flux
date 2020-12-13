9a587004deb13e307e8068ce84679b894fcf296f
az365 <artemcarr@yandex.ru>
2020-12-13 18:50:57 +0300

    AbstractConnector: get_name(), get_storage(), get_context(), get_names_hierarchy(), get_path(), get_meta() and more methods added

--- connectors/abstract.py
+++ connectors/abstract.py
@@ -1,3 +1,5 @@
+from abc import ABC, abstractmethod
+
 try:  # Assume we're a sub-module in a package.
     from utils import (
         arguments as arg,
@@ -15,17 +17,38 @@ DEFAULT_PATH_DELIMITER = '/'
 CHUNK_SIZE = 8192
 
 
-class AbstractConnector:
+class AbstractConnector(ABC):
     def __init__(
             self,
             name,
-            context=None,
+            parent=None,
     ):
         self.name = name
-        self.context = context
+        self.parent = parent
+
+    @staticmethod
+    def is_context():
+        return False
+
+    def is_root(self):
+        return self.get_parent().is_context()
+
+    @abstractmethod
+    def has_hierarchy(self):
+        pass
+
+    def get_name(self):
+        return self.name
+
+    def get_parent(self):
+        return self.parent
+
+    def get_storage(self):
+        if not self.is_root():
+            return self.get_parent().get_storage()
 
     def get_context(self):
-        return self.context
+        return self.get_parent().get_context()
 
     def get_logger(self):
         if self.get_context():
@@ -40,6 +63,36 @@ class AbstractConnector:
                 msg=msg, level=level,
                 end=end, verbose=verbose,
             )
+
+    def get_names_hierarchy(self):
+        if self.is_root():
+            hierarchy = list()
+        else:
+            hierarchy = self.get_parent().get_names_hierarchy()
+        return hierarchy + [self.get_name()]
+
+    def get_path_prefix(self):
+        return self.get_storage().get_path_prefix()
+
+    def get_path_delimiter(self):
+        return self.get_storage().get_path_delimiter()
+
+    def get_path(self):
+        if self.is_root():
+            return self.get_path_prefix()
+        else:
+            return self.get_parent().get_path() + self.get_path_delimiter() + self.get_name()
+
+    def get_path_as_list(self):
+        if self.is_root():
+            return [self.get_path_prefix()]
+        else:
+            return self.get_parent().get_path_as_list() + [self.name.split(self.get_path_delimiter())]
+
+    def get_meta(self):
+        meta = self.__dict__.copy()
+        meta.pop('context')
+        return meta
 
 
 class AbstractFolder:
